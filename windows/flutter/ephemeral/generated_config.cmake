7addc08dc1fd1eee33338565c4bb40f24f978941
grow-mate <rakshansnm07@gmail.com>
2024-10-23 06:52:08 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "G:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "G:\\safenow_v10\\safenowv10" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=G:\\flutter"
+  "PROJECT_DIR=G:\\safenow_v10\\safenowv10"
+  "FLUTTER_ROOT=G:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=G:\\safenow_v10\\safenowv10\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=G:\\safenow_v10\\safenowv10"
+  "FLUTTER_TARGET=G:\\safenow_v10\\safenowv10\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=G:\\safenow_v10\\safenowv10\\.dart_tool\\package_config.json"
+)
